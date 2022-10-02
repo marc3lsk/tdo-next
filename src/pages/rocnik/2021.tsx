c7716c56226765b65ff4257ba429bd00313996a4
Marcel Baláž <marcel.balaz@anasoft.sk>
2022-10-02 12:19:33 +0200

    rocnik 2021

---
+++ src/pages/rocnik/2021.tsx
@@ -0,0 +1,137 @@
+import Head from "next/head";
+import Article from "../../components/Article";
+import ImgHeader from "../../../public/rocnik/header2021.jpg";
+import ImgDen0 from "../../../public/rocnik/2021/0den.jpg";
+import ImgDen1 from "../../../public/rocnik/2021/1den.jpg";
+import ImgDen1Anus from "../../../public/rocnik/2021/1den-anus.jpg";
+import ImgDen2 from "../../../public/rocnik/2021/2den.jpg";
+import ImgDen3 from "../../../public/rocnik/2021/3den.jpg";
+import Nadpis from "../../components/Nadpis";
+import Galeria from "../../components/Galeria";
+import ObrazokGalerie from "../../components/ObrazokGalerie";
+import Odsek from "../../components/Odsek";
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>2021 - Tour de Orava</title>
+      </Head>
+      <Galeria>
+        <ObrazokGalerie {...ImgHeader} alt="Ročník 2021" className="w-full" />
+      </Galeria>
+      <Article>
+        <Nadpis prvy>0-tá etapa – 115 km</Nadpis>
+        <Galeria>
+          <ObrazokGalerie
+            {...ImgDen0}
+            alt="0-tá etapa - mapa a výškový profil"
+            className="mt-3 mb-5 w-full"
+          />
+        </Galeria>
+        <Odsek>
+          Na 0-tú etapu jubilejného 10-teho ročníka nastúpila štvorica Ľuboš B.,
+          Peťo, Mišo a Marcel. Z Nitry išli až po Čakajovce po novej paródii na
+          cyklotrasu, od Čakajoviec pokračovali v riadnej premávke až po
+          Partizánske. Odtiaľ to vzali po cyklotrase popri rieke Nitra a míňali
+          kameňolom, za ktorého koncom bola prírodná rezervácia, následne
+          kúpalisko a nakoniec odkalisko. Medzi Novákmi a Prievidzou museli
+          borci dokonca kvôli dažďu vytiahnuť na chvíľu bundy. Pri prechode z
+          Hornonitrianskej do Turčianskej kotliny bola v horskej chate Remata
+          plánová pauza na kávičku a nejaký ten zákusok, avšak kuchyňa bola
+          zatvorená a tak sa chalani museli uspokojiť len s kávičkou. Po kávičke
+          v strmom MTB-úseku museli všetci okrem Peťa chvíľu tlačiť bicykle.
+          Časť stúpania z Turčeka na Krahule Ľuboš a Marcel už poznali z ročníka
+          2018.
+        </Odsek>
+        <Nadpis>1 – etapa – 34 km</Nadpis>
+        <Galeria>
+          <ObrazokGalerie
+            {...ImgDen1}
+            alt="1 – etapa - mapa a výškový profil"
+            className="mt-3 mb-5 w-full"
+          />
+        </Galeria>
+        <Odsek>
+          Prvá etapa bola poňatá v rekreačnom duchu, išla sa kratšia trasa, ale
+          zato v náročnejšom teréne. Pelotón bol doplnený o Paľa, Juraja a Mira.
+          Pre Mira to znamenalo vôbec prvú účasť v etape TdO. Chalani išli
+          obklukou po MTB trase z Krahúľ na Skalku. Zo Skalky si užili zjazd do
+          Kremnice, kde si dali obed. Netýka sa to Mira s Ľubošom, ktorí išli z
+          logistických dôvodov priamo na chatu, aby ešte stihli nakúpiť proviant
+          na večernú grilovačku. S plnými žalúdkami to mali borci na chatu už
+          len do kopca. Ako prvý sa na chate objavil Peťo, ale prekvapujúco
+          nebol druhý Marcel, ale Mišo. Neskôr sa ukázalo, že Marcel dostal
+          defekt. Súboj o štvrté miesto bol súbojom ťažkých váh. Po ťažkom boji
+          si štvrté miesto vybojoval Paľo keď zdolal Juraja. Nakoľko bola chata
+          vybavená vírivkou a saunou, chalani zregenerovali anus priamo na chate
+          a tento ročník sa neuskutočnil typický sanitárny deň. Vo vírivke síce
+          plávalo geňo ale ani to nebránilo borcom vychutnať si v nej kapitánika
+          s kolou. Vodu ešte vylepšil Miro, ktorý sa pred vstupom do vírivky
+          neosprchoval, kedže ako tvrdil počas etapy sa ani nespotil. Večer bol
+          na programe ešte ilegálny pokrový turnaj.
+        </Odsek>
+        <Galeria>
+          <ObrazokGalerie
+            {...ImgDen1Anus}
+            alt="1 – etapa - anus"
+            className="mt-3 mb-5 w-full"
+          />
+        </Galeria>
+        <Nadpis>Kráľovská etapa - 71km</Nadpis>
+        <Galeria>
+          <ObrazokGalerie
+            {...ImgDen2}
+            alt="Kráľovská etapa - mapa a výškový profil"
+            className="mt-3 mb-5 w-full"
+          />
+        </Galeria>
+        <Odsek>
+          Pred etapou pomohol Paľo Marcelovi s opravou defektu. Pelotón ešte
+          doplnil Ľuboš K., avšak náročnosť etapy a obava zo splnenia časového
+          limitu spôsobila, že Miro radšej na etapu nenastúpil. Namiesto toho si
+          spravil sanitárny deň, zaspal na kúpalisku a spálil si hruď a stehná.
+          V zjazde z Krahúľ do Kremnice Mišo potvrdil povesť najlepšieho
+          zjazdára v pelotóne. Voľba trate bola pod záštitou Marcela, ktorému
+          Juro s Ľubošom K. vyčítali cestou do obce Lúčky kopce hneď za
+          Kremnicou, nakoľko im sľúbil rovinky. Marcelovou skratkou sa pelotón
+          dostal do Šašovského Podhradia. Odtiaľ nasledovalo ešte niekoľko
+          kilometrov relatívne jednoduchého profilu, ale od odbočky na Ihráč sa
+          cesta zdvihla a vytvorili sa skupinky podľa toho, komu aké tempo
+          vyhovovalo. V prvej skupine boli Peťo a Ľuboš B., ku ktorým sa ešte na
+          nejaký čas dotiahol Marcel. Ľuboš B. nemusel ľutovať, že deň predtým
+          schytal čierneho Petra a musel ísť na nákup, nakoľko aj v kráľovskej
+          etape čakalo na borcov stúpanie z Kremnice na Krahule. Peťo s Ľubošom
+          zastali na pivo približne 1 km pred chatou, do krčmy sa z chaty na
+          bicykli spustil aj spálený Miro a tak sa aspoň takto symbolicky
+          zúčastnil kráľovskej etapy. Marcel išiel z Kremnice stúpanie sám, ale
+          nakoľko sa nezastavil na pivo, bol na chate prvý. Druhú skupinu
+          vytvorili Paľo s Mišom. Táto skupina sa v Kremnici zastavila na
+          kávičku, čo využila tretia skupina – Juro a Ľuboš K. – a dostala sa
+          cez nich. Druhá skupina vyrazila z Kremnice až keď tretia skupina bolo
+          už približne v polovici stúpania a pokúsila sa ju dobehnúť. V tretej
+          skupine sa ale borci trápili, čo dávalo druhej skupine značné šance.
+          Ľuboš dokonca uvažoval, že odhodí bicykel do priekopy. Na začiatku
+          Krahúľ zbadal Juro rýchlo sa približujúcich borcov, nasadol ešte na
+          bicykel a pokúsil sa im ujsť, ale tempo druhej skupiny bolo zdrvujúce
+          a tretiu skupinu predbehla.
+        </Odsek>
+        <Nadpis>Epilóg – 118 km</Nadpis>
+        <Galeria>
+          <ObrazokGalerie
+            {...ImgDen3}
+            alt="Epilóg - mapa a výškový profil"
+            className="mt-3 mb-5 w-full"
+          />
+        </Galeria>
+        <Odsek>
+          Epilógu sa zúčastnili Ľuboš B., Mišo a Marcel. Z chaty až po odbočku
+          na Šašovské Podhradie doslova leteli, takmer po celý čas sa išlo z
+          kopca. Ukázalo sa, že Marcelová skratka v kráľovskej etape bola síce
+          dlhšia, ale o to horšia cesta. Ku kávičke v Žarnovici sa im tento krát
+          ušiel aj zákusok a od Tlmáč to bolo až po Nitru len hore a dole.
+        </Odsek>
+      </Article>
+    </>
+  );
+}

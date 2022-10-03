16d8d566117f55938cd6f7d8723eca166cfd2d45
Marcel Baláž <marcel.balaz@anasoft.sk>
2022-10-03 09:56:04 +0200

    wipe gutenberg css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className="lg:text-lg">
+      <body className="bg-gray-800 lg:text-lg">
         <Main />
         <NextScript />
       </body>

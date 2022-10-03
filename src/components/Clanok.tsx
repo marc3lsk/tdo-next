957a60b9cb10d159d352bcf937eebeb83d90ae6d
Marcel Baláž <marcel.balaz@anasoft.sk>
2022-10-03 17:25:30 +0200

    Article premenovane na Clanok

--- src/components/Clanok.tsx
+++ src/components/Clanok.tsx
@@ -1,4 +1,4 @@
-export default function Article({ children }) {
+export default function Clanok({ children }) {
   return (
     <div className="w-full bg-white">
       <article className="mx-auto max-w-3xl bg-white px-5 pt-8 pb-12 text-gray-700 lg:px-5">
